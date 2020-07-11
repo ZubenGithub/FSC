9416c072b5943afc7a2fd8019407ff798982d572
Zuben P. Brown <zb2218@cumc.columbia.edu>
2020-07-11 16:00:58 -0400

    initial commit

---
+++ graph_FSC_v0.1.py
@@ -0,0 +1,35 @@
+#!/home/zuben/miniconda3/bin/python3
+from lxml import etree
+import matplotlib.pyplot as plt
+
+import argparse
+
+# Read xml files
+X = []
+Y= []
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', type=str)
+
+args = parser.parse_args()
+
+title_and_filename = args.filename
+
+tree = etree.parse(title_and_filename)
+
+for Xvalues in tree.xpath("/fsc/coordinate/x"):
+  X.append(float(Xvalues.text))
+
+for Yvalues in tree.xpath("/fsc/coordinate/y"):
+  Y.append(float(Yvalues.text))
+
+plt.plot(X,Y)
+plt.xlabel("1/Resolution ($\AA$)")
+plt.ylabel("Correlation")
+
+plt.title(title_and_filename)
+
+plt.hlines(y=0.143, xmin=min(X), xmax=max(X), colors='red')
+plt.savefig("{0}.png".format(title_and_filename))
+
